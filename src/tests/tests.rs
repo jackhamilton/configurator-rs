84722a5acf6a3a69b709fb0439ba6ff889919779
Jack Hamilton <jackham800@gmail.com>
2025-10-02 17:01:21 -0500

    update

--- src/tests/tests.rs
+++ src/tests/tests.rs
@@ -1,32 +1,17 @@
 use toml::Table;
 use freezable_trait::freezable;
+use configurator_macros::config_builder;
 
-#[derive(Clone, Debug)]
-#[freezable]
-struct Config {
-    cache_file_location: String,
-    term_env_var_name: String,
-    nvim_plugin_env_var_name: String,
-    nvim_theme_env_var_name: String,
-    write_term_lua: bool,
-    term_lua_path: String,
-    theme: Vec<Theme>,
-}
-
-impl Default for Config {
-    fn default() -> Self {
-        Self {
-            cache_file_location: "~/.config/themester/.themecache".into(),
-            term_env_var_name: "TERM_THEME".into(),
-            nvim_theme_env_var_name: "NVIM_THEME".into(),
-            nvim_plugin_env_var_name: "NVIM_THEME_PLUGIN".into(),
-            write_term_lua: true,
-            term_lua_path: "~/.config/wezterm/dynamic_theme.lua".into(),
-            theme: vec![
-                Theme::default()
-            ]
-        }
-    }
+config_builder! {
+    cache_file_location: String = "~/.config/themester/.themecache".into(),
+    term_env_var_name: String = "TERM_THEME".into(),
+    nvim_plugin_env_var_name: String = "NVIM_THEME".into(),
+    nvim_theme_env_var_name: String = "NVIM_THEME_PLUGIN".into(),
+    write_term_lua: bool = true,
+    term_lua_path: String = "~/.config/wezterm/dynamic_theme.lua".into(),
+    theme: Vec<Theme> = vec![
+        Theme::default()
+    ],
 }
 
 #[derive(Clone, Debug)]
